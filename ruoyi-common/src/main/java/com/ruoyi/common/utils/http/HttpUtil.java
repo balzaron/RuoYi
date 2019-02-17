d22abf3d656807e02f9ee1094f1756f9ca2bafd3
高山岳 <shanyue.gao@hotmail.com>
2019-02-17 14:19:00 +0800

    http util modify.

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/http/HttpUtil.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/http/HttpUtil.java
@@ -21,6 +21,7 @@ import java.util.concurrent.TimeUnit;
  * @author: shanyue.gao
  * @date: 2019/1/22 下午3:17
  */
+
 @Component
 public class HttpUtil {
 
@@ -34,91 +35,10 @@ public class HttpUtil {
             .readTimeout(5, TimeUnit.SECONDS)
             .build();
 
-//    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-
-    private static void doPost(Response response) throws IOException {
-//        Headers headers = response.headers();
-//        for (int i = 0; i < headers.size(); i++) {
-//            logger.info( headers.name(i) + ":" + headers.value(i));
-//        }
-        logger.info( "onResponse: " + response.body().string());
-    }
-
-    public static Response get(String url){
-        Response response = null;
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        try {
-            response = client.newCall(request).execute();
-        }catch (IOException e){
-            logger.error(e.toString());
-        }
-        return response;
-    }
-
-
-    /**
-     * async post a file
-     * @param url
-     * @param filePath
-     */
-    public static void post(String url, String filePath) {
-        File file = new File(filePath);
-        Request request = new  Request.Builder()
-                .url(url)
-                .post(RequestBody.create(mediaType, file))
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-                logger.error(e.toString());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                logger.info(response.protocol() + " " +response.code() + " " + response.message());
-                doPost(response);
-            }
-        });
-    }
-
-//    /**
-//     * async post a pojo
-//     * @param url
-//     * @param obj
-//     */
-//    public void post(String url, Object obj) {
-//        String body = JSONObject.toJSONString(obj);
-//        Request request = new Request.Builder()
-//                .url(url)
-//                .post(RequestBody.create(mediaType, body))
-//                .build();
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                logger.error("onFailure: " + e.getMessage());
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                doPost(response);
-//            }
-//        });
-//    }
-
-    @Value("merchantNo")
+    @Value("sdxd.merchantNo")
     private String merchantNo;
 
-
-    public static String merchantNoTmp;
-    @Value("${smtpHost}")
-    public void setSmtpHost(String smtpHost) {
-        this.merchantNo = smtpHost;
-    }
-
-    public static <T, E> T post (String url, E parameter, Class<T> retClazz){
+    public <T, E> T post (String url, E parameter, Class<T> retClazz){
         MediaType mediaType = MediaType.parse("application/json");
         //使用JSONObject封装参数
         String par = JSON.toJSONString(parameter);
@@ -138,11 +58,9 @@ public class HttpUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        finally {
+
+        }
         return result;
     }
-
-    public static void main(String[] args) {
-
-    }
-
 }
